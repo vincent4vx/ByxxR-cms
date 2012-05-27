b89bb089ac14df8fa42ecf9820b90939f919e908
vincent4vx <=>
2012-05-27 14:51:09 +0200

    ajout de la fonction getHead (tools.php) => suppression du macro head (perso.html.twig

--- core/twig/extensions/tools.php
+++ core/twig/extensions/tools.php
@@ -11,7 +11,8 @@ class tools extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'pagination' => new \Twig_Function_Method($this, 'pagination', array('is_safe' => array('html')))
+            'pagination' => new \Twig_Function_Method($this, 'pagination', array('is_safe' => array('html'))),
+	    'getHead' => new \Twig_Function_Method($this, 'getHead')
         );
     }
     
@@ -37,5 +38,54 @@ class tools extends \Twig_Extension
             $return .= '<a href="'.$url.'?page='.($current + 1).'">&gt;</a> <a href="'.$url.'?page='.$end.'">&gt;&gt;</a>';
         }
         return $return;
+    }
+    
+    public function getHead($class, $sexe, $big = false)
+    {
+	switch ($class)
+	{
+	    case 1:
+		$str_class = 'feca';
+		break;
+	    case 2:
+		$str_class = 'osa';
+		break;
+	    case 3:
+		$str_class = 'enu';
+		break;
+	    case 4:
+	        $str_class = 'sram';
+		break;
+	    case 5:
+		$str_class = 'xel';
+		break;
+	    case 6:
+		$str_class = 'eca';
+		break;
+	    case 7:
+		$str_class = 'eni';
+		break;
+	    case 8:
+		$str_class = 'iop';
+		break;
+	    case 9:
+		$str_class = 'cra';
+		break;
+	    case 10:
+		$str_class = 'sadi';
+		break;
+	    case 11:
+		$str_class = 'sacri';
+		break;
+	    case 12:
+		$str_class = 'pand';
+		break;
+	    default :
+		$str_class = '';
+		break;
+	}
+	$str_class .= $sexe == 0 ? '_m' : '_f';
+	$path = $big ? 'class/' : 'heads/';
+	return $path.$str_class.'.png';
     }
 }
