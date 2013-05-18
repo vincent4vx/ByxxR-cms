5e278ffc9df3b58937448692d6fa1db795d2c3a3
vincent <vincent@vincent-Precision-WorkStation-390>
2013-05-18 23:42:11 +0200

    Ajout de la vue stats

---
+++ app/views/layouts/stats.html.php
@@ -0,0 +1,22 @@
+<div class="textContent">
+    <div id="cadre" style="background-image:url('<?php echo Core::conf('root').'public/images/cadres/backCadre_1.png'?>')">
+	<div style="position:absolute;margin-top:9px;">Statut :
+            <span style="color:#11ff00">En ligne</span>
+	</div>
+	<div style="position:absolute;margin-top:30px;">Comptes :
+            <b>452</b>
+	</div>
+	<div style="position:absolute;margin-top:52px;">Personnages :
+	    <b>459</b>
+	</div>
+	<div style="position:absolute;margin-top:73px;">Connectés :
+	    <b>42</b>
+	</div>
+	<div style="position:absolute;margin-top:94px;">Niveau moyen :
+	    <b>152</b>
+	</div>
+	<div style="position:absolute;margin-top:115px;">Leader :
+	    <b>xXRamboPlxX</b>
+	</div>
+    </div>
+</div>
