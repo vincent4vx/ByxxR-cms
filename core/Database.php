fa8ebd3590c37c537ca41ac3488543c367c0c314
vincent <vincent@vincent-Precision-WorkStation-390>
2013-05-22 21:38:20 +0200

    Database : encodage utf8

--- core/Database.php
+++ core/Database.php
@@ -17,7 +17,8 @@ class Database extends PDO
                     $this->_config['password'],
                     array(
                         PDO::ATTR_DEFAULT_FETCH_MODE=>PDO::FETCH_ASSOC,
-                        PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION
+                        PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION,
+                        PDO::MYSQL_ATTR_INIT_COMMAND=>'SET NAMES utf8'
                     )
             );
 	}catch(Exception $e)
