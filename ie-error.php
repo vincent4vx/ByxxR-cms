f8c1e1e409c5dce848698e449c793ce0ca864eda
vincent <vincent@vincent-Precision-WorkStation-390>
2013-06-22 13:27:25 +0200

    Restriction d'accès pour IE < 9

---
+++ ie-error.php
@@ -0,0 +1,43 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Navigateur incompatible !</title>
+    <style type="text/css">
+        body{
+            background: #03a;
+            color: #fff;
+        }
+        p{
+            padding-left: 25px;
+        }
+        h1{
+            background: #BABABA;
+            font-size: 16px;
+            color: #03a;
+            width: 125px;
+            font-weight: normal;
+            text-align: center;
+        }
+        #list{
+            margin: 50px;
+            font-size: 16px;
+        }
+        a {
+            color: #fff;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <center><h1>Internet explorer <?php echo $v?></h1></center>
+    <p>
+        Votre navigateur <b>(Microsoft Internet Explorer version <?php echo $v?>)</b> est obsolète, et n'est pas compatible avec <b>ByxxR CMS</b>.<br/>
+        Nous vous recommandons de le <u>mettre à jour</u>. Ce site est optimisé pour <b>Firefox, Chrome (webkit) et Opera</b>, pour une expérience optimale, il est conseillé de prendre plutôt un de ces navigateurs !<br/><br/>
+
+    <div id="list">
+        * Téléchargement d'Opera : <a href="http://www.opera.com">ici</a><br/>
+        * Téléchagement de Chrome : <a href="http://www.google.fr/intl/fr/chrome/browser/">ici</a><br/>
+        * Téléchargement de Firefox : <a href="http://www.mozilla.org/fr/firefox/new/">ici</a><br/>
+        * Mise à jour d'internet explorer : <a href="http://windows.microsoft.com/fr-fr/internet-explorer/download-ie">ici</a>
+    </div>
+    </p>
+</body>
--- index.php
+++ index.php
@@ -15,6 +15,17 @@ define('VERSION', '2.0a');
 define('DEBUG', true);
 define('START_TIME', microtime(true));
 
+$ua = $_SERVER['HTTP_USER_AGENT'];
+
+if(($pos=strpos($ua, 'MSIE '))!==false){
+    $v = (int)substr($ua, $pos + 5, 3);
+    if($v < 9){
+        require 'ie-error'.EXT;
+        exit;
+    }
+}
+
+
 setlocale(LC_TIME, 'fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
 
 require_once CORE.'Core'.EXT;
