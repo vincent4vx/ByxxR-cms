b9459fa50b706a8baacd58db0623f74f75f24df3
vincent <vincent@vincent-Precision-WorkStation-390>
2013-06-24 12:16:43 +0200

    resize auto tu nombre de points

--- app/views/layouts/menu/menurigth_member.html.php
+++ app/views/layouts/menu/menurigth_member.html.php
@@ -3,7 +3,7 @@
 </div>
 <ul>
 <li><a href="<?php echo Url::genUrl('user')?>"><?php echo Assets::img("devtool/zoom.png")?> Mon Profil</a></li>
-<li><a href="<?php echo Url::genUrl('points/buyPoints')?>"><?php echo Assets::img("devtool/cadeau.png")?> Mes Points (<b><small><?php echo $this->session->points?></small></b>)</a></li>
+<li><a href="<?php echo Url::genUrl('points/buyPoints')?>"><?php echo Assets::img("devtool/cadeau.png")?> Mes Points (<span style="font-weight: bold;font-size: <?php $s=1-(strlen($this->session->points)-2)/10;echo $s>1?1:$s;?>em;"><?php echo $this->session->points?></span>)</a></li>
 <!--<li><a href=""><?php echo Assets::img("devtool/bug.png")?> Mes Personnages</a></li>-->
 <!--{# <a href=""><li>{{ img("devtool/construction.png", "devtoolIcon") }} Déblocage</li></a>
 <a href=""><li>{{ img("devtool/cart.png", "devtoolIcon") }} Boutique</li></a> #}-->
