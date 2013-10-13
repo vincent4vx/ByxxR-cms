c93e5adc6364ec4be06852b760c6c7cbfb00c96a
vincent <vincent@vincent-Vostro-3560>
2013-10-13 13:15:39 +0200

    Optimisation du loacalstorage.js

--- public/js/localstorage.js
+++ public/js/localstorage.js
@@ -1,27 +1,25 @@
-(function(){
+(function() {
     var now, date;
     date = new Date();
     now = date.getTime();
 
-    if((localStorage.getItem('update_time') + 24 * 3600 * 1000) < now){
+    if ((localStorage.getItem('update_time') + 24 * 3600 * 1000) < now) {
         preload();
         localStorage.setItem('update_time', now + 24 * 3600 * 1000);
     }
 
-    $('img').each(function(){
+    $('img[data-url]').each(function() {
         var imgUrl = $(this).data('url');
-        if(imgUrl){
-            var data = localStorage.getItem(imgUrl);
-            if(!data)
-                $(this).attr('src', imgUrl);
-            else
-                $(this).attr('src', data);
-            $(this).removeAttr('data-url');
-        }
+        var data = localStorage.getItem(imgUrl);
+        if (!data)
+            $(this).attr('src', imgUrl);
+        else
+            $(this).attr('src', data);
+        $(this).removeAttr('data-url');
     });
 })();
 
-function preload(){
+function preload() {
     var preload = [
         'title/img_404.png',
         'title/img_accueil.png',
@@ -46,25 +44,25 @@ function preload(){
         'download.png'
     ];
 
-    $(preload).each(function(index, value){
+    $(preload).each(function(index, value) {
         var imgUrl = Url.baseUrl() + 'public/images/' + value;
         var data = localStorage.getItem(imgUrl);
 
         var xhr = new XMLHttpRequest(),
-            blob,
-            fileReader = new FileReader();
+                blob,
+                fileReader = new FileReader();
 
         xhr.open("GET", imgUrl, true);
         // Set the responseType to arraybuffer. "blob" is an option too, rendering BlobBuilder unnecessary, but the support for "blob" is not widespread enough yet
         xhr.responseType = "arraybuffer";
 
-        xhr.addEventListener("load", function () {
+        xhr.addEventListener("load", function() {
             if (xhr.status === 200) {
                 // Create a blob from the response
                 blob = new Blob([xhr.response], {type: "image/png"});
 
                 // onload needed since Google Chrome doesn't support addEventListener for FileReader
-                fileReader.onload = function (evt) {
+                fileReader.onload = function(evt) {
                     // Read out file contents as a Data URL
                     var result = evt.target.result;
                     // Store Data URL in localStorage
